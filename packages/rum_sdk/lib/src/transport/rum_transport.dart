b4e168a71bbfb2a7f5039e3f345da472b6101f1d
Robert Magnusson <robert@northmind.io>
2024-07-19 14:42:34 +0200

    Fix missing auth logic and broken example app

--- packages/rum_sdk/lib/src/transport/rum_transport.dart
+++ packages/rum_sdk/lib/src/transport/rum_transport.dart
@@ -6,28 +6,41 @@ import 'package:rum_sdk/src/transport/rum_base_transport.dart';
 import 'package:rum_sdk/src/transport/task_buffer.dart';
 import 'package:rum_sdk/src/models/payload.dart';
 
-
 class RUMTransport extends BaseTransport {
   final String collectorUrl;
   final String apiKey;
+  final String? sessionId;
   TaskBuffer<dynamic>? _taskBuffer;
-  RUMTransport({required this.collectorUrl, required this.apiKey, maxBufferLimit = 30 }) {
+
+  RUMTransport({
+    required this.collectorUrl,
+    required this.apiKey,
+    this.sessionId,
+    maxBufferLimit = 30,
+  }) {
     _taskBuffer = TaskBuffer(maxBufferLimit);
   }
 
   @override
   Future<void> send(Payload payload) async {
-    final headers = {'Content-Type': 'application/json', 'x-api-key': apiKey};
-    final response = await _taskBuffer?.add((){
-       return http.post(
-         Uri.parse(collectorUrl),
-         headers: headers,
-         body: jsonEncode(payload.toJson()),
-       );
+    final sessionId = this.sessionId;
+
+    final headers = {
+      'Content-Type': 'application/json',
+      'x-api-key': apiKey,
+      if (sessionId != null) 'x-faro-session-id': sessionId,
+    };
+    final response = await _taskBuffer?.add(() {
+      return http.post(
+        Uri.parse(collectorUrl),
+        headers: headers,
+        body: jsonEncode(payload.toJson()),
+      );
     });
-    if (response!= null && response?.statusCode ~/ 100 != 2) {
-          log("Error sending payload: ${response?.statusCode}");
+    if (response != null && response?.statusCode ~/ 100 != 2) {
+      log(
+        'Error sending payload: ${response?.statusCode}, body: ${response?.body}',
+      );
     }
   }
-
 }
